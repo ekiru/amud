1610415687d50c79b151de3455455cb778713837
Tyler Curtis <tcurtis@uchicago.edu>
2011-02-25 20:39:52 +0000

    Support very long send_messages and send in a non-blockingm manner.

--- src/messages/send.c
+++ src/messages/send.c
@@ -1,9 +1,12 @@
 #include "messages/send.h"
 
+#include <errno.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
+
+#include "error_status.h"
 
 typedef struct {
     message msg;
@@ -15,9 +18,30 @@ typedef struct {
 
 static void send_message_handler(message *msgp) {
     send_message_t *msg = (send_message_t *) msgp;
-    send(msg->fd, msg->text, msg->length, 0);
-    message_queue(msg->next);
-    free(msg);
+    ssize_t bytes_sent = send(msg->fd, msg->text, msg->length, MSG_DONTWAIT);
+    if (bytes_sent == msg->length) {
+        message_queue(msg->next);
+        free(msg);
+    } else if (bytes_sent >= 0) {
+        message *new_message = send_message(msg->fd, msg->text + bytes_sent, msg->next);
+        if (!new_message)
+            exit(ALLOCATION_ERROR);
+        message_queue(new_message);
+	free(msg);
+    } else {
+        switch (errno) {
+        case EAGAIN: /* message didn't send due to it being a blocking operation. */
+	    message_queue(msgp);
+            break;
+        case ECONNRESET:
+            /* need to work in handing of disconnections */
+            free(msg);
+            break;
+	default:
+            free(msg);
+            break;
+        }
+    }
 }   
 
 message *send_message(int fd, const char *text, message *next_message) {
